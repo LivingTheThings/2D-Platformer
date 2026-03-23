09fbe574f1028567f1ec69cbe3b967c5131f9eca
LivingTheThings <aafabreag@s.sfusd.edu>
2026-03-22 18:47:45 -0700

    Video Checkpoint 2

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,23 +14,25 @@ var move_input : float
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
 	move_input = Input.get_axis("move_left", "move_right")
-	velocity.x = move_input * move_speed
-	move_and_slide()	
 	
 	if move_input != 0:
 		velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, braking * delta)
-		
 	
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
+	
+	move_and_slide()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if velocity.x != 0:
 		sprite.flip_h = velocity.x > 0 
-		
+	
+	_manage_animation()
+
 func _manage_animation():
 	if not is_on_floor():
 		anim.play("jump")
@@ -38,4 +40,3 @@ func _manage_animation():
 		anim.play("move")
 	else:
 		anim.play("idle")
-	
